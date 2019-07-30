9d3c4af2ed23baa4886df0f8142899ef14cf6dcd
Danton Nguyen <dantonnguyen88@gmail.com>
2019-07-29 21:57:41 -0700

    complete project

---
+++ src/components/App/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './App.css'
+import Calculator from '../Calculator/Calculator'
+const App = () => {
+    return (
+        <div className="app-container">
+            <Calculator />
+        </div>
+    )
+}
+
+export default App
